c45b0f9656aea13e4f211527dbe62b489ea1da98
shishir8957 <85719050+Shishir8957@users.noreply.github.com>
2024-01-16 16:56:55 +0545

    update

---
+++ products/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+# try:
+  #     send_mail('Order Complete', f"Vehicle Booked order complete http://127.0.0.1:8000/product/productdetail/{id_product}/ ", 'royell4912@gmail.com', [request.user.email],fail_silently=False)
+  # except:
+  #   print('Error sending mail')
